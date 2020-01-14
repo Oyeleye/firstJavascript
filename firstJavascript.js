6ad733991031b74eb3fb6486a067f421d6af3a8f
Oyeleye <oyeleyeoyedayo@gmail.com>
2020-01-14 22:48:38 +0200

    first commit

---
+++ firstJavascript.js
@@ -0,0 +1,18 @@
+var userName = prompt("What is your name?");
+alert("Nice to meet you, " + userName + "!");
+console.log("Also great to meet you, " + userName);
+
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var fullName = (firstName + " " + lastName);
+alert("Your full name is " + fullName);
+console.log("Your full name is " + fullName);
+
+var age = prompt("How old are you?");
+alert("You are " + age + " years old.");
+console.log("You are " + age + " years old.");
+
+var yearsAlive = prompt("Enter your age and discover how many days you've been alive");
+var days = (yearsAlive * 365);
+alert("You have spent aproximately " + days + " days on earth.");
+console.log("You have spent aproximately " + days + " days on earth.");
